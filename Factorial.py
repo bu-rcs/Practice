06473ad00b02c67514a4649fac3bd4b87722b8f8
Katia Bulekova <ktrn@bu.edu>
2025-11-12 15:35:08 -0500

    Script to compute factorial

---
+++ Factorial.py
@@ -0,0 +1,9 @@
+# Write a Python script that computes factorial
+# of a given number using recursion.
+def factorial(n):
+    if n < 0:
+        raise ValueError("Factorial is not defined for negative numbers.")
+    elif n == 0 or n == 1:
+        return 1
+    else:
+        return n * factorial(n - 1)
